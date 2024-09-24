872aa6b23032b294686850aa29cdb65ae6f8b211
Suved G <vedghanmode@gmail.com>
2024-09-24 17:00:50 +0530

    Code to interface!

--- src/main/java/org/personal/multithreading/requests/SendMessage.java
+++ src/main/java/org/personal/multithreading/requests/SendMessage.java
@@ -1,19 +1,19 @@
 package org.personal.multithreading.requests;
 
-import org.personal.multithreading.message.OneToOneMessage;
+import org.personal.multithreading.message.IMessage;
 
 /**
  * This class represents a request to send a message.
  */
 public class SendMessage implements IRequest<Boolean> {
-    private final OneToOneMessage message;
+    private final IMessage message;
 
     /**
      * This constructor initializes the message to be sent.
      *
      * @param message The message to be sent.
      */
-    public SendMessage(OneToOneMessage message) {
+    public SendMessage(IMessage message) {
         this.message = message;
     }
 
